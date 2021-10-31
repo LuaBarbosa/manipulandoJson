d91751e443948c1b1400f0dad875d3808bd60071
LuaBarbosa <lua.barbosa@yahoo.com.br>
2021-10-31 11:37:51 -0300

    Feat: Incluindo o produto conforme estabelecimento

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 const fs = require("fs");
 
 //lê os conteúdos de um arquivo
-fs.readFile("./data.json", "utf-8", (error, data) => {
-  const objeto = JSON.parse(data);
-  console.log(objeto.products);
-});
+
+function saida() {
+  fs.readFile("./data.json", "utf-8", (error, data) => {
+    const objeto = JSON.parse(data);
+    let salvar = [];
+    
+
+    // const priceFormatado = parseInt(objeto.products.price);
+    // const price = priceFormatado / 100;
+    // console.log(priceFormatado);
+
+    objeto.establishments.map((item) => {
+      item.productsId.forEach((element) => {
+        const products = objeto.products.forEach((produto) => {
+          if (element === produto.id) {
+            salvar.push({
+              Establishment: item.name,
+              Categorias: categoria,
+              Products: produto.name,
+              Price: paserFloat(produto.price / 100),
+            });
+          }
+        });
+      });
+    });
+    console.log(salvar);
+  });
+}
+
+saida();
