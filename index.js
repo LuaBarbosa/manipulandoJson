efcb98780c00d624738fe06b12b6d900def4fcb1
LuaBarbosa <lua.barbosa@yahoo.com.br>
2021-11-01 12:17:10 -0300

    Feat: Incluindo dados

--- index.js
+++ index.js
@@ -3,42 +3,36 @@ const fs = require("fs");
 //lê os conteúdos de um arquivo
 
 fs.readFile("./data.json", "utf-8", (error, data) => {
-  const { products, establishments, categories } = JSON.parse(data);
+  const objeto = JSON.parse(data);
 
-  let produto = [];
-  let produtoId = [];
-  let produtos = [];
-  let categoriaId = [];
-  let categoriaNome = [];
-  let final = [];
+  let salvar = [];
 
-  function productsId(index) {
-    produtoId = establishments[index].productsId; //produtoId com produto
-    produtoId.forEach((element) => {
-      produto = products.find((p) => p.id === element);
-      produtos.push(produto);
-    });
+  let categoria = [];
 
-    produto.categoriesId.forEach((element2) => {
-      nomeCategoria = categories.find((cat) => cat.id === element2);
-      categoriaNome.push(nomeCategoria);
-    });
-    //console.log(categoriaNome);
-  }
-  productsId(1);
-
-  function categoriaProduto() {
-    categoriaNome.map((item) => {
-      produtos.forEach((element3) => {
-        if (element3.categoriesId.some((cat) => cat === item.id)) {
-          let saida = {
-            [item.name]: {
-              [element3.name]: {},
-            },
-          };
-          final.push(saida);
+  objeto.products.map((item) => {
+    item.categoriesId.forEach((element) => {
+      const categories = objeto.categories.forEach((categori) => {
+        if (categoria.includes(element)) {
+          return;
+        } else if (element === categori.id) {
+          categoria.push(categori.name);
         }
       });
     });
-  }
+  });
+
+  objeto.establishments.map((item) => {
+    item.productsId.forEach((element) => {
+      const products = objeto.products.forEach((produto) => {
+        if (element === produto.id) {
+          salvar.push({
+            Establishment: item.name,
+            Products: produto.name,
+            Categories: categoria,
+          });
+        }
+      });
+    });
+  });
+  console.log(salvar);
 });
